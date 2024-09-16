3fb0a91e4529af2ffadf93429ceb414f94ee1a30
Damageddream <marcan.grab@gmail.com>
2024-09-16 12:39:18 +0200

    first commit

---
+++ src/main/java/org/example/medicalclinicee/rest/PatientResource.java
@@ -0,0 +1,13 @@
+package org.example.medicalclinicee.rest;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/patient")
+public class PatientResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String
+}
